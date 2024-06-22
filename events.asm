dbd74649cc8019aa284b02b68b480545823c61cf
Sarah Shinespark <74634845+SarahShinespark@users.noreply.github.com>
2024-06-21 22:34:09 -0400

    Separate themes for logo and intro crawl; Darwin Ch4 joins with his theme

---
+++ events.asm
@@ -0,0 +1,25 @@
+;Events.asm
+;Holds modifications to the event assembly code
+
+;Skip unequimping Axs in the Lava Room. (This does nothing now, so there's 3 bytes free) (Turns out this skips kicking him altogether, but he still gets unequimped afterwards)
+;org $98AE39 : Equimp_Axs:
+;db $1A : dw $AE3C
+
+
+;Reorders two commands, so the check for dungeon BGM comes after Darwin rejoins in Chapter 4.
+org $98BB95 : Rejoin_Darwin_theme:
+db $1B : dw $D33D   ;Subroutine Darwin_joins
+db $07 : dl $83A520 ;Sub_Default_BGM
+
+
+;Restores the HAL Logo theme and injects the Crystal Sword theme into the intro text crawl
+org $9791D9 : Set_HAL_Logo_theme:
+db $07 : dl $809C3C : db $34    ;07: Call subroutine GetSet Music (Music ID: HAL Logo)
+
+org $9780D3 : Inject_CS_theme:
+db $04 : dl $97FEC5           ;ASM jump to free space
+
+org $97FEC5
+db $07 : dl $809C3C : db $45  ;Set BGM to Crystal Sword
+db $09 : dl $97826D           ;Background subroutine A_press_intro_crawl
+db $05                        ;Return long
