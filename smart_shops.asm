0d0e9cbe549f847360144e6ca3468306bf350061
Sarah Shinespark <74634845+SarahShinespark@users.noreply.github.com>
2024-07-23 23:16:51 -0400

    New with rapid card buying action!

--- smart_shops.asm
+++ smart_shops.asm
@@ -1,22 +1,20 @@
-;Jump point for Alchemist purchases; jumping back here brings you back into the drink menu immediately
-org $03BBAC : Alchemist_Menu:
+;Smart shops - Speeds up various purchases
 
-;After buying Water
-org $03BC1D
-db $1A           
-dw Alchemist_Menu
 
-;After buying Elixir
-org $03BC55
-db $1A           
-dw Alchemist_Menu
+;Jump point for Bartender purchases; jumping back here brings you back into the drink menu immediately
+org $03BBAC : Bartender_Menu:
 
-;After buying Food A
-org $03BC8D
-db $1A           
-dw Alchemist_Menu
+org $03BC1D : db $1A : dw Bartender_Menu    ;After buying Water, return to the food menu
+org $03BC55 : db $1A : dw Bartender_Menu    ;After buying Elixir
+org $03BC8D : db $1A : dw Bartender_Menu    ;After buying Food A
+org $03BCC5 : db $1A : dw Bartender_Menu    ;After buying Food B
 
-;After buying Food B
-org $03BCC5
-db $1A           
-dw Alchemist_Menu
+
+;Jump point for Card purchases; jumping back here lets you immediately buy another card.
+org $83C059 : CardBuy_Input_Check:
+
+org $83C0C3 : Card_Buy:
+db $07 : dl $809C44 : db $00               ;Call GetSet_SFX (none)
+db $07 : dl $809C44 : db $05               ;Call GetSet_SFX (ding)
+db $07 : dl $80A0AC : dl $88CCE4 : db $01  ;Call Text Reader (Shop menu GP, no screen clear)
+db $1A : dw CardBuy_Input_Check            ;Return to the input check, instead of exiting to "Anything Else?"
